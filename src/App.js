86225648d90042af612525a85d30fd698c2e1d3d
Sven Chen <sven.chen@lftech.com.tw>
2019-10-29 17:57:21 +0800

    non components ver.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Game({ game, index, removeGame }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classvalue="game">
+      {game.text}
+      <button onClick={() => removeGame(index)}>刪除</button>
+    </div>
+  );
+}
+
+function GameForm({ addGame }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addGame(value);
+    setValue('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="gameInput"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [games, setGames] = useState([{ text: '' }]);
+  const addGame = text => {
+    const newGames = [...games, { text }];
+    setGames(newGames);
+  };
+  const removeGame = index => {
+    const newGames = [...games];
+    newGames.splice(index, 1);
+    setGames(newGames);
+  };
+  return (
+    <div className="gameList">
+      <GameForm addGame={addGame} />
+      {games.map((game, index) => (
+        <Game key={index} index={index} game={game} removeGame={removeGame} />
+      ))}
     </div>
   );
 }
